e41952aa5d737f4ed3e22dd2e2e273894a6b5423
Berna <lucabusinesswork1@gmail.com>
2023-01-30 21:18:47 +0100

    Initial Commit

---
+++ LoginUi/LoginUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUiApp.swift
+//  LoginUi
+//
+//  Created by Luca Bernardis on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
